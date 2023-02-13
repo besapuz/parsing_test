7351f27129e51ee96d393837ce2ce751a02cc0a1
Алексндр Заринский <aleksandzarinskij18772@gmail.com>
2023-02-13 22:45:02 +0300

    изменил переменные на более читаемые

--- new.py
+++ new.py
@@ -13,17 +13,17 @@ URL = environ['URL']
 DOC = environ['DOC']
 WEY = environ['WEY']
 # Указываем путь к JSON
-gs = gspread.service_account(filename=WEY)
+gc = gspread.service_account(filename=WEY)
 # Открываем тестовую таблицу
-go = gs.open_by_url(DOC)
+sh = gc.open_by_url(DOC)
 
 response = requests.get(URL)
 soup = BeautifulSoup(response.text, 'lxml')
 quotes = soup.find_all('table')
 head = soup.find_all('th')
-td = soup.find_all('td', class_='confluenceTd')
+tg = soup.find_all('td', class_='confluenceTd')
 
-table = Table(gs, head, go)
+table = Table(tg, head, sh)
 
 table.update_head()
 table.update_values()
--- table.py
+++ table.py
@@ -1,28 +1,28 @@
 
 class Table:
-    __slots__ = ('gs', 'head', 'go', 'abc', 'string', 'column')
+    __slots__ = ('tg', 'head', 'sh', 'abc', 'string', 'column')
 
-    def __init__(self, gs, head, go, abc: str = 'ABCDEFG', string: int = 2, column: int = 0):
+    def __init__(self, tg, head, sh, abc: str = 'ABCDEFG', string: int = 2, column: int = 0):
         self.head = head
-        self.go = go
+        self.sh = sh
         self.abc = abc
         self.string = string
         self.column = column
-        self.gs = gs
+        self.tg = tg
 
     def update_head(self) -> None:
         for h in self.head:
             text = h.text
-            self.go.sheet1.update(f'{self.abc[self.column]}1', text)
+            self.sh.sheet1.update(f'{self.abc[self.column]}1', text)
             self.column += 1
         self.column = 0
 
     def update_values(self) -> None:
-        for quote in self.gs:
+        for quote in self.tg:
             text = quote.text
             if text.isdigit():
-                self.go.sheet1.update(f'A{self.string}', text)
+                self.sh.sheet1.update(f'A{self.string}', text)
             else:
-                self.go.sheet1.update(f'B{self.string}', text)
+                self.sh.sheet1.update(f'B{self.string}', text)
                 self.string += 1
         self.string = 2
